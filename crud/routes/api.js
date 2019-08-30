afe5e33e8dff783a218b6d5091c14ef86796b2c8
cherryApp <j.cserko@gmail.com>
2019-08-30 14:39:47 +0200

    Fix: comments.

--- crud/routes/api.js
+++ crud/routes/api.js
@@ -7,14 +7,14 @@ router.get('/', function (req, res, next) {
     res.send('Hi, this is the api page.');
 });
 
-// Example: /users/5 = server gives a users who has id = 5.
+// Example: /users/5 = user with id 5.
 router.get('/:entity/:id', async (req, res, next) => {
     const db = new DB(req.params.entity);
     let list = await db.find(req.params.id);
     res.json(list);
 });
 
-// Example: /users/ = server gives all of users.
+// Example: /users/ = all users.
 router.get('/:entity', async (req, res, next) => {
     const db = new DB(req.params.entity);
     let list = await db.find();
